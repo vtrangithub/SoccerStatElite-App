c0fb03406e419904cf7045cd582534115b02219b
vtrangithub <viettran@MacBook-Pro-14-M3-Pro.local>
2025-05-06 12:32:07 -0700

    Updated for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
+  base: "/SoccerStatElite-App/",
   plugins: [react()],
-})
+});
