1790747086f04dfc0a0705725361f65ddca41b19
vtrangithub <viettran@MacBook-Pro-14-M3-Pro.local>
2025-05-12 17:32:59 -0700

    Updated API key

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
